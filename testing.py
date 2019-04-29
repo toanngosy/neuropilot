f4a406f04e500ca32e0afdce7ed1509f86673872
Toanngo <toanngosy@gmail.com>
2019-04-29 09:23:39 +0200

    add custom reward wrapper example

--- testing.py
+++ testing.py
@@ -1,8 +1,21 @@
 import gym
 import quadrotorenv
 
-env = gym.make('QuadRotorEnv-v0')
+
+class CustomRewardWrapper(gym.RewardWrapper):
+    def __init__(self, env):
+        super(CustomRewardWrapper, self).__init__(env)
+
+    def reward(self, reward):
+        return 1
+
+
+env = CustomRewardWrapper(gym.make('QuadRotorEnv-v0'))
 state = env.reset()
-for i in range(100):
-    obs, _, _, _ = env.step(env.action_space.sample())
-    print(i, obs)
+
+done = False
+i = 0
+while not done:
+    i += 1
+    obs, reward, done, _ = env.step(env.action_space.sample())
+    print(reward, end=" ")
