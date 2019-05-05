1e455225f96d058938d459c31421fb77d72827e8
Toanngo <toanngosy@gmail.com>
2019-05-05 10:54:28 +0200

    hew env

--- quadrotorenv/__init__.py
+++ quadrotorenv/__init__.py
@@ -9,3 +9,13 @@ register(
     id='QuadRotorEnv-v1',
     entry_point='quadrotorenv.quadrotorenv:QuadRotorEnv_v1'
 )
+
+register(
+    id='SimpleTargetEnv2D-v0',
+    entry_point='quadrotorenv.quadrotorenv:SimpleTargetEnv2D'
+)
+
+register(
+    id='SimpleTargetEnv3D-v0',
+    entry_point='quadrotorenv.quadrotorenv:SimpleTargetEnv3D'
+)
--- quadrotorenv/quadrotorenv.py
+++ quadrotorenv/quadrotorenv.py
@@ -65,6 +65,9 @@ class QuadRotorEnv_v0(gym.Env):
         # set random seed
         self.seed()
 
+        # reset env
+        self.reset()
+
         # set viewer
         self.viewer = None
 
@@ -367,6 +370,9 @@ class QuadRotorEnv_v1(gym.Env):
         # set random seed
         self.seed()
 
+        # reset env
+        self.reset()
+
         # set viewer
         self.viewer = None
 
@@ -401,12 +407,12 @@ class QuadRotorEnv_v1(gym.Env):
         #self.reward_bin = np.array([0, d/3, 2*d/3, d])
         #self.reward_list = np.array([-0.75, -0.5, 0, 0.5])
         #dist_reward = self.reward_list[(np.digitize(d, self.reward_bin)-1)]
-        
+
         pose_reward = 1/((np.mean(abs(self.state[6:8]))/(np.pi))+0.5)
 
         time_reward = -0.01*self.num_step
         reward = dist_reward/10 + pose_reward + time_reward
-        
+
         if self._reach_target():
             self.target[0] = np.random.uniform(low=self.min_x, high=self.max_x)
             self.target[1] = np.random.uniform(low=self.min_y, high=self.max_y)
@@ -633,4 +639,48 @@ class Propeller:
         self.speed = 0
         self.thrust = 0
 
+class SimpleTargetEnv2D(gym.Env):
+    def __init__(self):
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(2,), dtype=np.float32)
+        self.action_space = gym.spaces.Box(low=-0.1, high=0.1, shape=(2,), dtype=np.float32)
+        self.reset()
 
+    def reset(self):
+        self.state = np.random.uniform(-1, 1, size=(2,))
+        observation = np.copy(self.state)
+        return observation
+
+    def step(self, action):
+        self.state = self.state + action
+        x, y = self.state
+        reward = - (x**2 + y**2) ** 0.5
+        done = abs(x) < 0.01 and abs(y) < 0.01
+        next_observation = np.copy(self.state)
+        print(self.state, reward)
+        return next_observation, reward, done, {}
+
+    def render(self, mode='human', close=False):
+        print('current state:', self.state)
+
+class SimpleTargetEnv3D(gym.Env):
+    def __init__(self):
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32)
+        self.action_space = gym.spaces.Box(low=-0.1, high=0.1, shape=(3,), dtype=np.float32)
+        self.reset()
+
+    def reset(self):
+        self.state = np.random.uniform(-1, 1, size=(3,))
+        observation = np.copy(self.state)
+        return observation
+
+    def step(self, action):
+        self.state = self.state + action
+        x, y, z = self.state
+        reward = - (x**2 + y**2 + z**2) ** 0.5
+        done = abs(x) < 0.01 and abs(y) < 0.01
+        next_observation = np.copy(self.state)
+        print(self.state, reward)
+        return next_observation, reward, done, {}
+
+    def render(self, mode='human', close=False):
+        print('current state:', self.state)
