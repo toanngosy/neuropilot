c52e9f2d64cf3af4ef4956bb885b91c99a51d85e
unknown <qhuy.phm@gmail.com>
2019-05-05 13:31:57 +0200

    relocate file and add rendering to simpletarget env

--- envs/simpletargetenv.py
+++ envs/simpletargetenv.py
@@ -4,6 +4,7 @@ from gym.utils import seeding
 import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import mpl_toolkits.mplot3d.axes3d as Axes3D
 import sys
 
@@ -13,6 +14,24 @@ class SimpleTargetEnv2D(gym.Env):
         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(2,), dtype=np.float32)
         self.action_space = gym.spaces.Box(low=-0.1, high=0.1, shape=(2,), dtype=np.float32)
         self.reset()
+
+        #defind enviroment bound 
+        self.minx = 0
+        self.maxx = 10
+        self.miny = 0
+        self.maxy = 10
+
+        #initialize rendering
+        self.fig, self.ax = plt.subplots()
+        self.ani = animation.FuncAnimation(self.fig, self.render, interval = 1, init_func = self.setup_plot, blit = True)
+
+
+    def setup_plot(self):
+    	x,y = self.state
+    	self.ax.plot([self.minx, self.maxx, self.maxx, self.minx, self.minx], [self.miny,self.miny,self.maxy,self.maxy, self.miny], color = 'black', label = 'Wall')
+    	self.scat = self.ax.scatter(x,y, animated = True)
+    	self.ax.axis([0.9*self.minx, 1.1*self.maxx, 0.9*self.miny, 1.1*self.maxy])
+    	self.ax.legend()
 
     def reset(self):
         self.state = np.random.uniform(-1, 1, size=(2,))
@@ -29,7 +48,10 @@ class SimpleTargetEnv2D(gym.Env):
         return next_observation, reward, done, {}
 
     def render(self, mode='human', close=False):
-        print('current state:', self.state)
+        position = self.state
+        self.scat.set_offsets(position)
+
+        return self.scat,
 
 
 class SimpleTargetEnv3D(gym.Env):
